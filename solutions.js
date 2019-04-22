2495b7b184ebc663c210d3e2db2fbb1dd4d10499
Christian <christiankhawam@outlook.com>
2019-04-22 08:13:31 -0400

    Vowel Count

--- solutions.js
+++ solutions.js
@@ -123,3 +123,18 @@ function solution(number) {
     return sum
 }
 
+
+
+/* Vowel Count -- Kata Level 7
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+//Solution
+function getCount(str) {
+    let m = str.match(/[aeiou]/gi)
+    return m.length
+}
